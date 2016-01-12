beb170dbcb7a52f2c7b1be23387d6a954913779c
Karb0 <carbonel.mathieu@gmail.com>
2016-01-12 16:51:52 +0100

    inverse test

--- test.rb
+++ test.rb
@@ -3,31 +3,31 @@ require 'minitest/autorun'
 class TestNucleo < Minitest::Test
 
   def test_1
-    assert_equal Nucleo.new("AA").symb_count, "2 0 0 0"
+    assert_equal "2 0 0 0", Nucleo.new("AA").symb_count 
   end
 
   def test_2
-    assert_equal Nucleo.new("AC").symb_count, "1 1 0 0"
+    assert_equal "1 1 0 0", Nucleo.new("AC").symb_count 
   end
 
   def test_3
-    assert_equal Nucleo.new("GT").symb_count, "0 0 1 1"
+    assert_equal "0 0 1 1", Nucleo.new("GT").symb_count 
   end
 
   def test_4
-    assert_equal Nucleo.new("ACGT").symb_count, "1 1 1 1"
+    assert_equal "1 1 1 1", Nucleo.new("ACGT").symb_count 
   end
 
   def test_5
-    assert_equal Nucleo.new("ACTTGACGT").symb_count, "2 2 2 3"
+    assert_equal "2 2 2 3", Nucleo.new("ACTTGACGT").symb_count 
   end
 
   def test_6
-    assert_equal Nucleo.new("ATTGCTAGCTAA").symb_count, "4 2 2 4"
+    assert_equal "4 2 2 4", Nucleo.new("ATTGCTAGCTAA").symb_count 
   end
 
   def test_7
-    assert_equal Nucleo.new("ATGCTTCAGAAAAGGTCAGCG").symb_count, "7 4 6 4"
+    assert_equal "7 4 6 4", Nucleo.new("ATGCTTCAGAAAAGGTCAGCG").symb_count 
   end
 
 end
